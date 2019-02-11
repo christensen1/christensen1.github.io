19d737153626d553266ebdfe38be5520d762b8ec
christensen1 <47329978+christensen1@users.noreply.github.com>
2019-02-11 11:01:48 -0600

    febuary 11, primative shapes

---
+++ primitive paint/sketch.js
@@ -0,0 +1,27 @@
+// Primitive Paint
+// Leif CHristensen
+// Febuary 11, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+let sizeV = 50
+let sizeH = 50
+
+
+function setup() {
+  createCanvas(2000, 2000);
+  background(0);
+}
+keyTyped();
+
+function keyTyped(){
+  if (key === 'a'){
+      rect(mouseX-25, mouseY-25, sizeV, sizeH );
+  } 
+  if (key === 's' ){
+      ellipse(mouseX, mouseY, sizeV, sizeH );
+  }
+  if (key === 'd'){
+    triangle(mouseX + 30,mouseY +30, mouseX - 30, mouseY + 30,mouseX, mouseY-30);
+  }
+}
