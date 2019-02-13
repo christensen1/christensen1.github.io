efe895bc298837321113dc68a7a1bf6ce59c7113
christensen1 <47329978+christensen1@users.noreply.github.com>
2019-02-13 11:02:49 -0600

    febuary 13, 2019

--- primitive paint/sketch.js
+++ primitive paint/sketch.js
@@ -4,8 +4,9 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let sizeV = 50
-let sizeH = 50
+let sizeV = 50;
+let sizeH = 50;
+
 
 
 function setup() {
@@ -16,12 +17,15 @@ keyTyped();
 
 function keyTyped(){
   if (key === 'a'){
+      fill(random(1,255), random(1,255), random(1,255))
       rect(mouseX-25, mouseY-25, sizeV, sizeH );
   } 
   if (key === 's' ){
+      fill(random(1,255), random(1,255), random(1,255))
       ellipse(mouseX, mouseY, sizeV, sizeH );
   }
   if (key === 'd'){
+    fill(random(1,255), random(1,255), random(1,255))
     triangle(mouseX + 30,mouseY +30, mouseX - 30, mouseY + 30,mouseX, mouseY-30);
   }
 }
--- varible demo/sketch.js
+++ varible demo/sketch.js
@@ -1,10 +1,16 @@
-// Project Title
-// Your Name
-// Date
+// Varible demo
+// Leif christensen
+// feb 13, 2019
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let state = 0;
+const rectSize = 20;
+let x = 0;
+let y = 0;
+
+//zero stands for top, one stands for right two will be bottom and three will stand for left.
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,4 +18,13 @@ function setup() {
 
 function draw() {
   background(220);
+  if (state === 0 ){
+    x += 10;
+    if (x > windowWidth - rectSize) state = 1;
+
+  }
+  else if (state === 0 ){
+
+  }
+  rect(0, 0, rectSize, rectSize);
 }
