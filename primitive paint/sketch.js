ee071b1a9ad08cbf57654fbce14318a140f84af5
christensen1 <47329978+christensen1@users.noreply.github.com>
2019-02-14 10:35:49 -0600

    primative shapes: feb 14 , 2019

--- primitive paint/sketch.js
+++ primitive paint/sketch.js
@@ -4,29 +4,43 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
 let sizeV = 50;
 let sizeH = 50;
 
 
 
 function setup() {
-  createCanvas(2000, 2000);
+  createCanvas(windowWidth, windowHeight);
   background(0);
+  fill(74,234,56);
+  textSize(20);
+  text("LEIF CHRISTNENSEN", windowWidth - 425, windowHeight - 100);
 }
 keyTyped();
 
+function draw(){
+
+}
+
 function keyTyped(){
   if (key === 'a'){
-      fill(random(1,255), random(1,255), random(1,255))
-      rect(mouseX-25, mouseY-25, sizeV, sizeH );
+    fill(random(1,255), random(1,255), random(1,255))
+    rect(mouseX-25, mouseY-25, sizeV, sizeH );
   } 
   if (key === 's' ){
-      fill(random(1,255), random(1,255), random(1,255))
-      ellipse(mouseX, mouseY, sizeV, sizeH );
+    fill(random(1,255), random(1,255), random(1,255))
+    ellipse(mouseX, mouseY, sizeV, sizeH );
   }
   if (key === 'd'){
     fill(random(1,255), random(1,255), random(1,255))
     triangle(mouseX + 30,mouseY +30, mouseX - 30, mouseY + 30,mouseX, mouseY-30);
+  }
+  if (keyCode === 32){
+    background(0);
+    fill(74,234,56);
+    textSize(20);
+    text("LEIF CHRISTNENSEN", windowWidth - 425, windowHeight - 100);
   }
 }
 
